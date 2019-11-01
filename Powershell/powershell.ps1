ca64f9b92933b02b11f6d760053668bd054ae147
Praveen Dasan <d.p.praveen@gmail.com>
2019-11-01 22:16:00 +0800

    Chapter 02 done

--- Powershell/powershell.ps1
+++ Powershell/powershell.ps1
@@ -1,1 +1,10 @@
-Get-ChildItem
+$food = 'Global scope variable'
+function Myfunc {
+    $food = 'Function local variable'
+    Write-Host $Global:food
+    Write-Host $Local:food
+    Write-Host $food
+}
+Myfunc
+Write-Host $Local:food
+Write-Host $food
